50d84fe4d10ea5c204ad03379ea5fd5d83ea3c8f
Santasiero, William <william_santasiero@homedepot.com>
2017-06-09 21:16:43 -0400

    Add devtools for hot reload
    IDEA requires some setting changes, however

--- src/main/resources/static/js/kendoUI.js
+++ src/main/resources/static/js/kendoUI.js
@@ -1,4 +1,4 @@
-angular.module('kendo', [ "kendo.directives" ])
+angular.module('kendo', [])
     .controller('home', function($scope) {
         $scope.greeting = {id: 'xxx', content: 'Hello World!'}
     })
