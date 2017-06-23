2ced3a094351c1771b0529e81471a3f874424ace
Santasiero, William <william_santasiero@homedepot.com>
2017-06-23 10:31:12 -0400

    Bring in ui-router

--- src/main/resources/static/js/kendoUI.js
+++ src/main/resources/static/js/kendoUI.js
@@ -1,4 +1,24 @@
-angular.module('kendo', ["kendo.directives"])
-    .controller('home', function($scope) {
-        $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+angular.module('kendo', ["kendo.directives", "ui.router"])
+    .config(function($stateProvider) {
+        var helloState = {
+            name: 'hello',
+            url: '/hello',
+            template: '<h3>hello world!</h3>'
+        }
+
+        var aboutState = {
+            name: 'about',
+            url: '/about',
+            template: '<h3>Its the UI-Router hello world app!</h3>'
+        }
+
+        $stateProvider.state(helloState);
+        $stateProvider.state(aboutState);
     })
+    .controller('home', function($scope, $state) {
+        $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+        $scope.showDataVis = function(){
+            $state.transitionTo('hello');
+        };
+    });
+
